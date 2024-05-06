738df0f600b1e922f41f3ff2f629a8705bb817a8
Russ Gunther <russell.g@leantaas.com>
2024-05-06 17:47:35 -0400

    Chap 5: Refactor calculator loop to use private function with pattern matching on differnt message

--- 05_concurrency_primitives/calculator.ex
+++ 05_concurrency_primitives/calculator.ex
@@ -6,28 +6,36 @@ defmodule Calculator do
   defp loop(current_value) do
     new_value =
       receive do
-        {:value, caller_pid} ->
-          send(caller_pid, {:response, current_value})
-          current_value
-
-        {:add, value} ->
-          current_value + value
-
-        {:sub, value} ->
-          current_value - value
-
-        {:mul, value} ->
-          current_value * value
-
-        {:div, value} ->
-          current_value / value
-
-        invalid_request ->
-          IO.puts("invalid request #{inspect(invalid_request)}")
-          current_value
+        message -> process_message(current_value, message)
       end
 
     loop(new_value)
+  end
+
+  defp process_message(current_value, {:value, caller_pid}) do
+    send(caller_pid, {:response, current_value})
+    current_value
+  end
+
+  defp process_message(current_value, {:add, value}) do
+    current_value + value
+  end
+
+  defp process_message(current_value, {:sub, value}) do
+    current_value - value
+  end
+
+  defp process_message(current_value, {:mul, value}) do
+    current_value * value
+  end
+
+  defp process_message(current_value, {:div, value}) do
+    current_value / value
+  end
+
+  defp process_message(current_value, invalid_request) do
+    IO.puts("invalid request #{inspect(invalid_request)}")
+    current_value
   end
 
   def value(server_pid) do
@@ -43,3 +51,11 @@ defmodule Calculator do
   def mul(server_pid, value), do: send(server_pid, {:mul, value})
   def div(server_pid, value), do: send(server_pid, {:div, value})
 end
+
+calculator_pid = Calculator.start()
+Calculator.value(calculator_pid)
+Calculator.add(calculator_pid, 10)
+Calculator.sub(calculator_pid, 5)
+Calculator.mul(calculator_pid, 3)
+Calculator.div(calculator_pid, 5)
+Calculator.value(calculator_pid)

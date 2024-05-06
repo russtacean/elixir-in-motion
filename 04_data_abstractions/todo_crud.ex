b8a7d48b7aa34a96cdae920acc4e86740b9151c1
Russ Gunther <russell.g@leantaas.com>
2024-05-05 20:53:38 -0400

    Chap 4: Implement Collectable protocol for TodoList

--- 04_data_abstractions/todo_crud.ex
+++ 04_data_abstractions/todo_crud.ex
@@ -60,3 +60,16 @@ defmodule TodoList.CsvImporter do
     %{date: date, title: title}
   end
 end
+
+defimpl Collectable, for: TodoList do
+  def into(original) do
+    {original, &into_callback/2}
+  end
+
+  defp into_callback(todo_list, {:cont, entry}) do
+    TodoList.add_entry(todo_list, entry)
+  end
+
+  defp into_callback(todo_list, :done), do: todo_list
+  defp into_callback(_todo_list, :halt), do: :ok
+end

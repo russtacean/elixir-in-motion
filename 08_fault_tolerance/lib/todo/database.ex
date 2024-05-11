ce63998571aa079e0436251f601c7e88f25f055b
Russ Gunther <russell.g@leantaas.com>
2024-05-11 18:07:26 -0400

    Chap 8: Link all processes to prevent dangling processes from being left over when the cache is killed

--- 08_fault_tolerance/lib/todo/database.ex
+++ 08_fault_tolerance/lib/todo/database.ex
@@ -3,8 +3,8 @@ defmodule Todo.Database do
 
   @db_folder "./persist"
 
-  def start do
-    GenServer.start(__MODULE__, nil, name: __MODULE__)
+  def start_link() do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
   def store(key, data) do
@@ -33,7 +33,7 @@ defmodule Todo.Database do
 
   defp init_workers() do
     for idx <- 0..2, into: %{} do
-      {:ok, pid} = Todo.DatabaseWorker.start(@db_folder)
+      {:ok, pid} = Todo.DatabaseWorker.start_link(@db_folder)
       {idx, pid}
     end
   end
--- 08_fault_tolerance/lib/todo/server.ex
+++ 08_fault_tolerance/lib/todo/server.ex
@@ -1,8 +1,8 @@
 defmodule Todo.Server do
   use GenServer
 
-  def start(name) do
-    GenServer.start(__MODULE__, name)
+  def start_link(name) do
+    GenServer.start_link(__MODULE__, name)
   end
 
   def add_entry(todo_server, new_entry) do

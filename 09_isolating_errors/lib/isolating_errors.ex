e7312002e9cba3f3a6a942658fa9378a542dd47e
Russ Gunther <russell.g@leantaas.com>
2024-05-11 18:11:16 -0400

    Chap 09: Base commit from last chapter

---
+++ 09_isolating_errors/lib/isolating_errors.ex
@@ -0,0 +1,18 @@
+defmodule IsolatingErrors do
+  @moduledoc """
+  Documentation for `IsolatingErrors`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> IsolatingErrors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end

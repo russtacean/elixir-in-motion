5a8f4f279a2ad325b2c9c7adce28c01a0cdfb5d2
Russ Gunther <russell.g@leantaas.com>
2024-05-11 22:04:28 -0400

    Chap 11: Update mix.exs and add application module for full OTP application compliance

--- 10_beyond_genserver/test/isolating_errors_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule IsolatingErrorsTest do
-  use ExUnit.Case
-  doctest IsolatingErrors
-
-  test "greets the world" do
-    assert IsolatingErrors.hello() == :world
-  end
-end
--- 11_otp_applications_and_webserver/test/todo/cache_test.exs
+++ 11_otp_applications_and_webserver/test/todo/cache_test.exs
@@ -2,16 +2,14 @@ defmodule Todo.CacheTest do
   use ExUnit.Case
 
   test "server_process" do
-    {:ok, cache} = Todo.Cache.start()
-    bob_pid = Todo.Cache.server_process(cache, "bob")
+    bob_pid = Todo.Cache.server_process("bob")
 
-    assert bob_pid != Todo.Cache.server_process(cache, "alice")
-    assert bob_pid == Todo.Cache.server_process(cache, "bob")
+    assert bob_pid != Todo.Cache.server_process("alice")
+    assert bob_pid == Todo.Cache.server_process("bob")
   end
 
   test "to-do operations" do
-    {:ok, cache} = Todo.Cache.start()
-    alice_pid = Todo.Cache.server_process(cache, "alice")
+    alice_pid = Todo.Cache.server_process("alice")
     Todo.Server.add_entry(alice_pid, %{date: ~D[2024-05-01], title: "Dentist"})
 
     entries = Todo.Server.entries(alice_pid, ~D[2024-05-01])

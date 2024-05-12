0657de11e732673866430447622c2fe3ef4db127
Russ Gunther <russell.g@leantaas.com>
2024-05-11 23:07:28 -0400

    Chap 11: Set different http port depending on mix env

--- 11_otp_applications_and_webserver/config/runtime.exs
+++ 11_otp_applications_and_webserver/config/runtime.exs
@@ -1,4 +1,8 @@
 import Config
 
-http_port = System.get_env("TODO_HTTP_PORT", "5454")
-config :todo, http_port: String.to_integer(http_port)
+http_port =
+  if config_env() != :test,
+    do: System.get_env("TODO_HTTP_PORT", "5454"),
+    else: System.get_env("TODO_TEST_HTTP_PORT", "5455")
+
+config(:todo, http_port: String.to_integer(http_port))
